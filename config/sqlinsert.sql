8aba14e28d7f430ea49ee0cf3b83472d76a01c8c
Kleon Kita <kk@cockpit360.co>
2013-07-19 13:59:54 -0400

    Signed-off-by: Kleon Kita <kk@cockpit360.co>

--- config/sqlinsert.sql
+++ config/sqlinsert.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-	'superadmin' tinyint(1) NOT NULL DEFAULT 0,
+	`superadmin` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`userID`, `username`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
